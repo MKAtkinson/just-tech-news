a3f36fc96af7206e6b1bbf06cc7373cfadc37179
MKAtkinson <maxatkinson@ymail.com>
2022-08-01 15:57:45 -0500

    Revert "finish encryption and password validation"
    This reverts commit 9cb9800dca2dad96d9f169754b3cb9d3145a1593.

--- models/User.js
+++ models/User.js
@@ -2,11 +2,7 @@ const bcrypt = require('bcrypt')
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-      }
-    }    
+class User extends Model {}
 
 User.init(
   {
